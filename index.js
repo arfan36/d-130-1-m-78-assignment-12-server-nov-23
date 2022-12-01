71331786d204959e6c46605d1bb50d80b38b16f2
arfan36 <arfanur36@gmail.com>
2022-12-01 11:22:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 6000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// mongoDb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clusterarfan36.opuzllc.mongodb.net/?retryWrites=true&w=majority`;
+console.log('uri :>> ', uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.get('/', (req, res) => {
+    res.send('Server running');
+});
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+});
