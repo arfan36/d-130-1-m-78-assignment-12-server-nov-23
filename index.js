d9901f8610f8efcc235a641aead80ccaf48027f4
arfan36 <arfanur36@gmail.com>
2022-12-02 12:32:48 +0600

    home category info added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 7000;
 
 // middleware
 app.use(cors());
@@ -12,8 +12,24 @@ app.use(express.json());
 
 // mongoDb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clusterarfan36.opuzllc.mongodb.net/?retryWrites=true&w=majority`;
-console.log('uri :>> ', uri);
+// console.log('uri :>> ', uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categoryCollection = client.db('assignment12').collection('homeCategories');
+
+        // Read categories
+        app.get('/categories', async (req, res) => {
+            res.send(await categoryCollection.find({}).toArray());
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
